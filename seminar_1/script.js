4a1ae09fe40c51d9cafed37d2ae120302c66f87f
Andrey Narushev <narushevai@gmail.com>
2023-01-21 22:03:55 +0500

    seminar_1

---
+++ seminar_1/script.js
@@ -0,0 +1,8 @@
+let tcel = 10;
+let tfr = (9 / 5) * tcel + 32;
+console.log(`Температура в градусах Цельсия = ${tcel}
+Температура в градусах Фаренгейта = ${tfr}`);
+
+let name = "Андрей";
+let admin = name;
+console.log(admin);
