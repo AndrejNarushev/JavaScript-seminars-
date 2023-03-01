0f760c1f1048b3fb8997a10ab6bbe50821611a28
Andrey Narushev <narushevai@gmail.com>
2023-03-01 23:27:22 +0500

    seminar_12

---
+++ seminar_12/js/data.js
@@ -0,0 +1,23 @@
+const productData =[
+    {
+        src:"./img/чай.jpg",
+        alt:"изображение чая",
+        href:"../html/tea.html",
+        name:"видео чай",
+        option: "черный чай",
+    },
+    {
+        src:"./img/кофе.jpg",
+        alt:"изображение кофе",
+        href:"../html/cofe.html",
+        name:"видео кофе",
+        option: "крепкий кофе"
+    },
+    {
+        src:"./img/компот.jpg",
+        alt:"изображение компота",
+        href:"../html/compot.html",
+        name:"видео компот",
+        option:"горячий компот"
+    }
+]
